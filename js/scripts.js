fcb59c676ce3a113c6ed1c4161e1e35d69594bc8
kenna warsinske <kennawarsinske@gmail.com>
2015-04-10 14:35:19 -0700

    add readme

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,42 @@
 var triangle = function(a, b, c) {
-  var triangleType = "";
-  parseInt(a);
-  parseInt(b);
-  parseInt(c);
-
-  console.log(a);
 
   //Invalid triangle check
   //check that each side isn't more than the length of the other two
   //also checks if a side is 0 or less
   //checks if user input was "string"
-  if ( ((a + b)<=c) || ((c + b) <= a) || ((a + c) <= b) || ((a || b || c) <= 0) || (typeof (a || b || c) === "string") ) {
-    triangleType = "NOT A TRIANGLE";
+  //weirdly, typeof only works in tests. using isNaN instead. typeof included for reference.
+
+  if ( ((a + b)<=c) || ((c + b) <= a) || ((a + c) <= b) || ((a || b || c) <= 0) || (typeof (a || b || c) === "string") || isNaN(a) || isNaN(b) || isNaN(c) ) {
+    return "NOT A TRIANGLE";
   }
 
   //equalateral
     else if ((a === b) && (b === c)){
-      triangleType = "EQUILATERAL";
+      return "EQUILATERAL";
     }
 
   //iscosceles
     else if (((a === b) && (b != c)) || ((a === c) && (b != c)) || ((b === c) && (a != b))) {
-      triangleType = "ISOSCELES";
+      return "ISOSCELES";
     }
 
   //scalene
     else{
-      triangleType = "SCALENE";
+      return "SCALENE";
     }
 
-
-  return triangleType;
 }
 
 $(document).ready(function() {
   $("form#sides").submit(function(event){
-  var a = $("input#sideA").val();
-  var b = $("input#sideB").val();
-  var c = $("input#sideC").val();
+  var a = parseInt($("input#sideA").val());
+  var b = parseInt($("input#sideB").val());
+  var c = parseInt($("input#sideC").val());
 
-  var result = triangle(a,b,c);
-  debugger;
+  var result = triangle(a, b, c);
+  $("#type").text(result);
+  $("#results").show();
+  event.preventDefault();
   });
 
 });
-
-
-
-
-
-//.
